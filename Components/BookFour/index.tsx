ac9de20f4f7c789a7fe6ac816e2a183ca227b949
Malin Cumpanasu <malin.c20@gmail.com>
2022-06-29 17:54:47 +0300

    babel

---
+++ Components/BookFour/index.tsx
@@ -0,0 +1,10 @@
+export default function BookFour() {
+  return (
+    <div className="book-four">
+      <div className="book-info">
+        <p className="title">Forth Book</p>
+        <p className="author">X</p>
+      </div>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,21 @@ import type { NextPage } from "next";
 import BookOne from "../Components/BookOne";
 import BookTwo from "../Components/BookTwo";
 import BookThree from "../Components/BookThree";
+import BookFour from "../Components/BookFour";
 
 const Home: NextPage = () => {
   return (
     <>
-      <h1>Cu si despre carti</h1>
       <div className="the-bookshelf">
+        <h1>
+          Cu <br /> si <br /> despre carti
+        </h1>
         <div className="bookshelf-wrap">
           <div className="bookshelf">
             <BookOne />
             <BookTwo />
             <BookThree />
+            <BookFour />
           </div>
         </div>
       </div>
